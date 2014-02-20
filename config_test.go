e884317a6839cc404e5a5e6236e1d5273d161f91
aquilax <aquilax@gmail.com>
2014-02-20 09:41:01 +0100

    Added config test

---
+++ config_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	fileName := "";
+	config := NewConfig()
+	err := config.Load(fileName)
+	if err == nil {
+		t.Error("Expected nil got", err)
+	}
+}
